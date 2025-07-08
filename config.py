0d8c2d5e1e7a85bf47ca94083bf2a003aabe2975
Christian Valencia <christianval27@gmail.com>
2025-07-08 14:54:42 -0400

    Pushing application and restoring DB with backup

--- config.py
+++ config.py
@@ -1,20 +1,14 @@
 import os
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SECRET_KEY = os.environ.get('SECRET_KEY', 'your-default-secret-key')
 
-    # SQLAlchemy Database URI
-    SQLALCHEMY_DATABASE_URI = (
-        
-"postgresql://postgres:DevOps2024*@database-cdb-3375-final-project.cl2gqc6scvwd.ca-central-1.rds.amazonaws.com:5432"
+    SQLALCHEMY_DATABASE_URI = os.environ.get(
+        "DATABASE_URL", 
+        "postgresql://postgres:DevOps2024*@localhost:5432/webappdb"
     )
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-    # Flask-Session configuration (if used)
     SESSION_TYPE = "filesystem"
-
-
-
-
 
 
